5bf8f7c918d5faec78a0cdc5b0a9c76a4ca25993
IKobozev <IKobozev@synergy.ru>
2022-03-22 01:09:45 +0300

    text add land

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <nav>
+      <a href="">Домашняя страница</a>
+      <a href="">Пользователи</a>
+      <a href="">Мой пользователь</a> 
+    </nav>
+    <h1>{{ msg }}</h1>
+    <h2>{{ number }}</h2>
+    <button v-on:click="add"> add nimber</button>
+    <p>
+      Документацию можно рассмотреть здесь
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return{
+      number: "1",
+    };
+  },
+  methods:{
+    add(){
+    this.number = this.number + this.number;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  margin-top: 100px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+nav{
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-evenly;
+}
+a {
+  color: #42b983;
+}
+</style>
